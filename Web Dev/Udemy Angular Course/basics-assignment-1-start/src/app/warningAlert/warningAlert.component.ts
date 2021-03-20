5e5ca1be3e2c1090bb4306f19b2c73a76e2e9393
WafeHorse <migcatana@gmail.com>
2021-03-20 16:33:40 +0000

    Udemy Assigment 1

---
+++ Web Dev/Udemy Angular Course/basics-assignment-1-start/src/app/warningAlert/warningAlert.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-warningAlert',
+  templateUrl: './warningAlert.component.html',
+  styles: ['p { color: red }']
+})
+export class WarningAlertComponent {
+
+}
