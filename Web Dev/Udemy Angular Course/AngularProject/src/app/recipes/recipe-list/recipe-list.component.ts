ec458803e457b9efa7781532446d7047c9ea2684
WaveHorse <migcatana@gmail.com>
2021-06-23 20:46:07 +0100

    + Angular Project (2nd part)

--- Web Dev/Udemy Angular Course/AngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Web Dev/Udemy Angular Course/AngularProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,15 +7,21 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is simple a test', 'https://cookieandkate.com/images/2018/03/simple-marinara-sauce-recipe-4-550x824.jpg'),
-    new Recipe('A Test Recipe', 'This is simple a test', 'https://cookieandkate.com/images/2018/03/simple-marinara-sauce-recipe-4-550x824.jpg')
+    new Recipe('Another Test Recipe', 'This is simple a test', 'https://cookieandkate.com/images/2018/03/simple-marinara-sauce-recipe-4-550x824.jpg')
 
   ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
